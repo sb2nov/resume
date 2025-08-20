68272c21d85534802779abb9ca9583c64ebd35fd
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-08-20 22:22:52 +0200

    new cv template init

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t sb2nov/latex .
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
--- latexmkrc
+++
@@ -1,2 +0,0 @@
-# Speed up latexmk-xelatex compilation re pdfx
-$hash_calc_ignore_pattern{'timestamp'} = '^';
